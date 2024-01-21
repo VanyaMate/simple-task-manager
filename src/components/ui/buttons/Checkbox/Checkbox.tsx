8b6245ba569c4abffcce04b3088d6f026b45aeb6
VanyaMate <buhu653@mail.ru>
2024-01-21 09:36:52 +0300

    fix for lint

--- src/components/ui/buttons/Checkbox/Checkbox.tsx
+++ src/components/ui/buttons/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@/components/ui/buttons/Button/Button.tsx';
 import IconM from '@/components/ui/icons/IconM/IconM.tsx';
 
